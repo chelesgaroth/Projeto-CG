291c01f326a29a47ca8a86ff24b98366b4018bcb
rachelesgaroth <raquelsofiamcosta@gmail.com>
2021-03-10 00:42:56 +0000

    little edit in -help menu

--- Fase1/src/Gerador/gerador.cpp
+++ Fase1/src/Gerador/gerador.cpp
@@ -69,7 +69,7 @@ int main(int argc, char* argv[]) {
     figure f;
 
     //Gerar os vértices para o desenho do plano e transcrever para o ficheiro .3d
-    if((strcmp(argv[1],"plane")==0)&&(argc==5)) {
+    if((strcmp(argv[1],"Plane")==0)&&(argc==5)) {
 
         float x = std::stof(argv[2]);
         float z = std::stof(argv[3]);
@@ -82,7 +82,7 @@ int main(int argc, char* argv[]) {
     }
 
         //Gerar os vértices para o desenho do cubo/caixa e transcrever para o ficheiro .3d
-    else if((strcmp(argv[1],"box")==0)&&(argc==7)){
+    else if((strcmp(argv[1],"Box")==0)&&(argc==7)){
         float x = std::stof(argv[2]);
         float y = std::stof(argv[3]);
         float z = std::stof(argv[4]);
@@ -98,7 +98,7 @@ int main(int argc, char* argv[]) {
     }
 
         //Gerar os vértices para o desenho da esfera e transcrever para o ficheiro .3d
-    else if((strcmp(argv[1],"sphere")==0)&&(argc==6)){
+    else if((strcmp(argv[1],"Sphere")==0)&&(argc==6)){
         float radius = std::stof(argv[2]);
         stringstream aux(argv[3]);
         int slices = 0;
@@ -116,7 +116,7 @@ int main(int argc, char* argv[]) {
     }
 
         //Gerar os vértices para o desenho do cone e transcrever para o ficheiro .3d
-    else if((strcmp(argv[1], "cone")==0)&&(argc==7)){
+    else if((strcmp(argv[1], "Cone")==0)&&(argc==7)){
         float radius = std::stof(argv[2]);
         float height = std::stof(argv[3]);
         stringstream aux(argv[4]);
@@ -136,10 +136,10 @@ int main(int argc, char* argv[]) {
 
         //Tela de ajuda e comandos
     else if(strcmp(argv[1], "-help")==0){
-        std::cout << "Plane         [x] [y]\n"
-                     "Box           [x] [y] [z] [divisions per edge]\n"
-                     "Sphere        [radius] [slices] [stacks]\n"
-                     "Cone          [radius] [height] [slices] [stacks]\n" << std::endl;
+        std::cout << "Plane         [x] [y] [file.3d]\n"
+                     "Box           [x] [y] [z] [divisions per edge] [file.3d]\n"
+                     "Sphere        [radius] [slices] [stacks] [file.3d]\n"
+                     "Cone          [radius] [height] [slices] [stacks] [file.3d]\n" << std::endl;
     }
     else {
         std::cout <<"\nMissing arguments\n"<< std::endl;
